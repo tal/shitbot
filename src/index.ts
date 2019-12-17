733ef4b5b361ff5df98e0bd2d3cadb970bf000c5
Tal Atlas <me@tal.by>
2019-12-17 15:17:21 -0500

    export loggers

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { Logger, LogLevel } from '@slack/logger'
+
 import { MessageHandler } from './handler-set'
 import { RandomAccessSet } from './random-access-set'
 import { RoundRobinSet } from './round-robin-set'
@@ -15,4 +17,6 @@ export {
   MessageHandler, // Mainly for typings
   Matcher, // Mainly for typings
   Message,
+  Logger,
+  LogLevel,
 }
